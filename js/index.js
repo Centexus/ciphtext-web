a7c09046740a5eff6cd845481389c1d12873099a
Rishikesh <rkesh2003@gmail.com>
2020-12-27 12:29:24 +0530

    desiged form

---
+++ js/index.js
@@ -0,0 +1,9 @@
+const shiftNum = document.querySelector("#shift");
+const message = document.querySelector("#message");
+const result = document.querySelector("#result");
+function encrypt() {}
+function decrypt() {
+  const shiftVal = shiftNum.value;
+
+  const messageVal = message.value;
+}
